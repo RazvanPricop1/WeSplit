dcab820f231e99ff8ef9ca99244f0928657deac1
Razvan Pricop <razvanipricop@gmail.com>
2024-09-29 19:16:19 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Razvan Pricop on 29.09.24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
